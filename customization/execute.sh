8842beda06563381ee847401e9f7b4c0850058ae
Rodrigo Miguele <rodrigomiguele@gmail.com>
2016-08-25 09:27:49 -0300

    Checking how much time a server spend to start

--- customization/execute.sh
+++ customization/execute.sh
@@ -9,6 +9,7 @@ JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 BATCH_FILE=$1
 JBOSS_MODE=${2:-"domain"}
 JBOSS_CONFIG=${3:-"$JBOSS_MODE.xml"}
+MAX_WAIT=60
 
 if [ "$JBOSS_MODE" = "domain" ]; then
     MASTER_HOST=`grep 'host name="MASTER_HOST_NAME"' $JBOSS_HOME/domain/configuration/host.xml`
@@ -20,18 +21,41 @@ if [ "$JBOSS_MODE" = "domain" ]; then
 fi
 
 if [ "x$BATCH_FILE" = "x" ]; then
-    echo "You need to inform the batch file to execute"
+    echo "ERROR! You need to inform the batch file to execute"
     exit 1
 fi
 
+function shutdown_jboss() {
+    if [ "$JBOSS_MODE" = "standalone" ]; then
+        $JBOSS_CLI -c ":shutdown"
+    else
+        $JBOSS_CLI -c "/host=$MASTER:shutdown"
+    fi
+}
+
 function wait_for_server() {
+    STARTED_AT="$(date +%s)"
     if [ "$JBOSS_MODE" = "standalone" ]; then
             until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
                     sleep 1
+                    NOW="$(date +%s)"
+                    TIME_UNTIL_NOW=`expr $NOW - $STARTED_AT`
+                    if [ $TIME_UNTIL_NOW -gt $MAX_WAIT ]; then
+                        echo "ERROR! The server didn't started after $MAX_WAIT. Shutting down..."
+                        shutdown_jboss
+                        exit 1
+                    fi
             done
     else
             until `$JBOSS_CLI -c "/host=$MASTER:read-attribute(name=host-state)" 2> /dev/null | grep -q running`; do
                     sleep 1
+                    NOW="$(date +%s)"
+                    TIME_UNTIL_NOW=`expr $NOW - $STARTED_AT`
+                    if [ $TIME_UNTIL_NOW -gt $MAX_WAIT ]; then
+                        echo "ERROR! The server didn't started after $MAX_WAIT. Shutting down..."
+                        shutdown_jboss
+                        exit 1
+                    fi
             done
     fi
 }
@@ -42,12 +66,8 @@ $JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG > /dev/null &
 echo "=> Waiting for the server to boot"
 wait_for_server
 
-echo "=> Executing the commands"
+echo "=> Executing the commands from file $BATCH_FILE"
 $JBOSS_CLI -c --file=$BATCH_FILE
 
 echo "=> Shutting down JBOSS"
-if [ "$JBOSS_MODE" = "standalone" ]; then
-    $JBOSS_CLI -c ":shutdown"
-else
-    $JBOSS_CLI -c "/host=$MASTER:shutdown"
-fi
+shutdown_jboss
